5e693ab01ab26c3d131f88591c3a7c99ee806265
dovjas <dovydas.jaseliunas@gmail.com>
2026-01-23 08:53:18 +0200

    CF debug

--- features/pageobjects/common.page.ts
+++ features/pageobjects/common.page.ts
@@ -3,6 +3,7 @@ class commonPage{
         await browser.url('https://practicesoftwaretesting.com/');
         const html = await browser.getPageSource();
         console.log('IS THIS CF?????????',html.substring(0, 2000));
+        await browser.saveScreenshot('cf.png');
         await console.log('Page opened:https://practicesoftwaretesting.com/');
 
         await browser.waitUntil(
