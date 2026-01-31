03e9d852c5a04109f9108cefd10468c6d4bd4263
dovjas <dovydas.jaseliunas@gmail.com>
2026-01-31 10:47:37 +0200

    removed CF logs

--- features/pageobjects/common.page.ts
+++ features/pageobjects/common.page.ts
@@ -1,18 +1,7 @@
 class CommonPage{
     openHomePage = async()=>{
         await browser.url('https://practicesoftwaretesting.com/');
-        const html = await browser.getPageSource();
-        console.log('IS THIS CF?????????',html.substring(0, 2000));
-        await browser.saveScreenshot('cf.png');
         await console.log('Page opened:https://practicesoftwaretesting.com/');
-
-        await browser.waitUntil(
-          async () => (await browser.getTitle()) !== 'Just a moment...',
-          {
-            timeout: 20000,
-            timeoutMsg: 'Cloudflare protection page did not disappear',
-          },
-        );
     }
 }
 
